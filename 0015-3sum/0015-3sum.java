4ab91e5afbb04ba98ed0d2b7a7bc7b84295322aa
JIUNG GU <60885635+JIUNG9@users.noreply.github.com>
2024-11-23 09:18:07 +0900

    Time: 30 ms (66.7%), Space: 51.2 MB (75.52%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,39 +1,39 @@
 import java.util.*;
+
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        
-        Arrays.sort(nums);
         List<List<Integer>> answerList = new ArrayList<>();
         int len = nums.length;
+        Arrays.sort(nums);
 
+        for (int i = 0; i < len; i++) {
+            // Skip duplicates for the first number
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
 
-        for(int i = 0; i < len; i++){
-            if (i > 0 && nums[i] == nums[i-1]) {
-                continue;
-            }
-            int sum = nums[i];
-            int left = i+1;
-            int right = len -1;
-            while(left < right){
-                if(sum + nums[left] + nums[right] == 0){
-                    answerList.add(List.of(nums[i],nums [left], nums[right]));
+            int left = i + 1;
+            int right = len - 1;
+
+            while (left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
+
+                if (sum == 0) {
+                    answerList.add(Arrays.asList(nums[i], nums[left], nums[right]));
+
+                    // Skip duplicates for the second number
+                    while (left < right && nums[left] == nums[left + 1]) left++;
+                    // Skip duplicates for the third number
+                    while (left < right && nums[right] == nums[right - 1]) right--;
+
                     left++;
-                    while(left < len && nums[left-1] == nums[left]){
-                        left++;
-                    }
-                }
-                else if(sum + nums[left] + nums[right] < 0){
+                    right--;
+                } else if (sum < 0) {
                     left++;
-                }
-                else{
+                } else {
                     right--;
                 }
-
             }
         }
 
         return answerList;
     }
-
-    
 }

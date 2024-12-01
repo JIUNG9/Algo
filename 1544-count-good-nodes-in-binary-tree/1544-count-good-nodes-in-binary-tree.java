b900e02b130353b5c4884f95ab242c826c9c4a0d
JIUNG GU <60885635+JIUNG9@users.noreply.github.com>
2024-12-01 09:05:52 +0900

    Time: 2 ms (85.29%), Space: 52.7 MB (17.97%) - LeetHub

--- 1544-count-good-nodes-in-binary-tree/1544-count-good-nodes-in-binary-tree.java
+++ 1544-count-good-nodes-in-binary-tree/1544-count-good-nodes-in-binary-tree.java
@@ -18,17 +18,19 @@ class Solution {
     public int goodNodes(TreeNode root) {
         //현재 노드에서 모든 것을 탐색하고(최단 거리가 아니라서 -> DFS 사용 모든 것을 탐색) 이때, 현재 노드보다, 같거나 큰 노드로 이동했다면 정답을 하나 증가시키고, 해당 정답 값을 반환한다.
         //root to X -> there are no nodes with the value greater than X
-        dfs(null, root,root.val);
+        dfs(root,root.val);
         return answer;
     }
 
-    public void dfs(TreeNode prev, TreeNode crnt, int biggest){
-        //모든 노드를 방문하는데, 이전에 방문했던 것보다 값이 크거나 같다면 정답 크기를 하나 증가 -> ㅇ
-        if(prev == null || biggest <= crnt.val) {
+    public void dfs(TreeNode crnt, int biggest){
+        if(crnt == null) return;
+        if(biggest <= crnt.val) {
             answer++;
+            biggest = crnt.val;
         }
-        if(crnt.left!=null) dfs(crnt, crnt.left, biggest >= crnt.left.val ? biggest : crnt.left.val);
-        if(crnt.right!=null) dfs(crnt, crnt.right, biggest >= crnt.right.val ? biggest : crnt.right.val);
+
+        dfs(crnt.left, biggest);
+        dfs(crnt.right, biggest);
     }
 
 

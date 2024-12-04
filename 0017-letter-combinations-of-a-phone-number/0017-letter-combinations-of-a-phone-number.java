6603374291674ed786adf066df3af89afa980350
JIUNG GU <60885635+JIUNG9@users.noreply.github.com>
2024-12-04 22:22:23 +0900

    Time: 1 ms (55.07%), Space: 42.2 MB (42.55%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
@@ -2,6 +2,9 @@ class Solution {
     public List<String> letterCombinations(String digits) {
         String[] pat ={"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
         List<String> answerList = new ArrayList<>();
+        //append 하는 방식 -> 기존의 존재하는 list에 digits의 각 문자별로 존재하는 String pat의 문자를 하나씩 리스트에 넣는다. 이후 다음 문자 또한 String의 존재하는 문자를 이미 존재했던 리스트에 전부 추가하고, 다른 문자 또한 모두 추가한다. 
+        //Impl -> 
+
         if(digits.length()==0) return answerList;
 
         answerList.add("");
@@ -14,7 +17,7 @@ class Solution {
                     temp.add(st + String.valueOf(ch));
                 }       
             }
-            answerList = new ArrayList<>(temp);
+            answerList = temp;
         }
         return answerList;
     }

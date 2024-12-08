a3f56764c8aa57125fb3b5e2fab296d05077f1dc
JIUNG GU <60885635+JIUNG9@users.noreply.github.com>
2024-12-08 10:53:40 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.java
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.java
@@ -1,16 +1,29 @@
 class Solution {
     public boolean uniqueOccurrences(int[] arr) {
-        Map<Integer,Integer> map = new HashMap<>();
-        for(int i : arr){
-            map.put(i, map.getOrDefault(i,0)+1);
-        }
 
-        List<Integer> list = map.values().stream().sorted((i1,i2) ->  i1-i2).collect(Collectors.toList());
-        for(int i = 1; i < list.size(); i++){
-            if(list.get(i-1).equals(list.get(i))){
-                return false;
+
+        if(arr.length == 2 && arr[0] != arr[1]) return false;
+
+        int[] freq = new int[1001];
+        Arrays.sort(arr);
+        int idx = 1;
+
+        while(idx < arr.length){
+            int count = 1;
+            while(idx < arr.length && arr[idx-1] == arr[idx]){
+                idx++;
+                count++;
             }
+            freq[count]++;
+            if(freq[count]!=1) return false;
+            idx++;
         }
+        
         return true;
+     
+
+
+        }
+            
+
     }
-}

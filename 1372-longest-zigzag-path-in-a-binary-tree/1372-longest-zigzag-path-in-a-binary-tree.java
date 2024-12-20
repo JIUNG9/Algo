f491dd9603100ddfed6eee8b9a1d49ebe96c6d63
JIUNG GU <60885635+JIUNG9@users.noreply.github.com>
2024-12-20 20:40:05 +0900

    Time: 4 ms (95.7%), Space: 55.3 MB (30.96%) - LeetHub

--- 1372-longest-zigzag-path-in-a-binary-tree/1372-longest-zigzag-path-in-a-binary-tree.java
+++ 1372-longest-zigzag-path-in-a-binary-tree/1372-longest-zigzag-path-in-a-binary-tree.java
@@ -1,63 +1,25 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
+    private int maxZigZag = 0;
+
     public int longestZigZag(TreeNode root) {
-        int answer= 0;
-        Queue<Node> q = new LinkedList<>();
-        if(root.left !=null) q.add(new Node(Dir.left,1,root.left));
-        if(root.right !=null)  q.add(new Node(Dir.right,1, root.right));
-
-        while(!q.isEmpty()){
-            Node n = q.poll();
-            if(n.treeNode!=null){
-                if(n.d.equals(Dir.left)){
-                    if(n.treeNode.right !=null) q.offer(new Node(Dir.right,n.cnt+1,n.treeNode.right));
-                    if(n.treeNode.left !=null) q.offer(new Node(Dir.left,1,n.treeNode.left));
-                } 
-                else {
-                    if(n.treeNode.left !=null) q.offer(new Node(Dir.left,n.cnt+1,n.treeNode.left));
-                    if(n.treeNode.right !=null) q.offer(new Node(Dir.right,1,n.treeNode.right));
-  
-                }
-                answer = Math.max(n.cnt, answer);
-            }
-
-            }
-        
-        return answer;
-        
-    }
-    class Node{
-        private Dir d;
-        private int cnt;
-        private TreeNode treeNode;
-
-        public Node(Dir d, int cnt, TreeNode treeNode){
-            this.cnt = cnt;
-            this.d = d;
-            this.treeNode = treeNode;
-        }
-        @Override
-        public String toString(){
-            return d +", " +  cnt +", " + treeNode;
-        }
-
+        dfs(root, -1, 0); // Start without any direction
+        return maxZigZag;
     }
 
-    enum Dir{
-        left,right;
+    private void dfs(TreeNode node, int direction, int length) {
+        if (node == null) return;
+        maxZigZag = Math.max(maxZigZag, length);
+        
+        if (direction != 0) { // If not coming from the left, go left
+            dfs(node.left, 0, length + 1);
+        } else {
+            dfs(node.left, 0, 1); // Reset length when changing direction
+        }
+        
+        if (direction != 1) { // If not coming from the right, go right
+            dfs(node.right, 1, length + 1);
+        } else {
+            dfs(node.right, 1, 1); // Reset length when changing direction
+        }
     }
 }

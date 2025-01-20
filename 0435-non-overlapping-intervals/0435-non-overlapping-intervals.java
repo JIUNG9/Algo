ab0e51ed12dcc0d5da950ddadf6bf33de38352ba
JIUNG GU <60885635+JIUNG9@users.noreply.github.com>
2025-01-20 16:46:24 +0900

    Time: 61 ms (12.86%), Space: 73 MB (97.35%) - LeetHub

--- 0435-non-overlapping-intervals/0435-non-overlapping-intervals.java
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals.java
@@ -1,45 +1,24 @@
 class Solution {
     public int eraseOverlapIntervals(int[][] intervals) {
-        //입력값 length 1
-        //겹치는 것을 지운 것의 최소 개수
-        //overlapped -> 한 지점의 시작점과 끝 지점 사이에 다른 점의 시작점이 존재하는 것
-        //[1,2],[1,3] 중 어떤 것을 잘라내야할까? 긴 길이를 가진 것은 겹칠 확률이 더 높다. 즉 항상 짧은 것을 잘라내지 않는 것이 이득이다. -> !!잘라낸 다음 업데이트를 하지 않았음 !!
-        //시작점을 기준으로 정렬하고 시작점이 같다면, 끝지점이 짧은 것을 기준으로 정렬한 이후, 지정했던 끝지점보다 현재 확인하는 다른 시작점이 더 작다면 해당 것이 존재할 때까지 다 잘라내고 정답 값도 증가 -> 만약 겹치는 것이 없다면, endpoint를 현재 검사하는 원소의 끝 부분으로 업데이트
-
-        /**
-        pseudo code
-        sort by start point asc, when the start point is same sort by end point asc as well
-
-        int endpoint = intervals[0][1];
-        for(1 to len){
-            if(endpoint > intervals[i][0]){
+        int answer = 0;
+        Arrays.sort(intervals,(a,b)-> {
+            if(a[1] == b[1]){
+                return b[0] - a[0];
+            }
+            return a[1] - b[1];
+        });
+        int idx = 1;
+        int[] criteria = intervals[0];
+        while(idx < intervals.length){
+            if(criteria[1] > intervals[idx][0]){
+                idx++;
                 answer++;
-                continue;
             }
             else{
-                endpoint = intervals[i][1];
+                criteria = intervals[idx++];
             }
         }
 
-         */
-         //겹친다는 것은 끝지점으로 기준으로 다른 시작점이 끝지점보다 작은 것을 의미한다. 즉 끝지점을 기준으로 오름차순 정렬했을 때, 그 다음에 오는 입력 값이 항상 현재 끝지점보다 크므로 현재 끝지점이 가장 작은 끝지점을 의미한다.
-
-         int answer = 0;
-         Arrays.sort(intervals,(arr1,arr2)->{
-            return arr1[0] - arr2[0];
-         });
-        int endpoint = intervals[0][1];
-        
-         for(int i = 1; i < intervals.length; i++){
-            if(endpoint > intervals[i][0]){
-                answer++;
-                endpoint = Math.min(endpoint, intervals[i][1]);
-            }
-            else{
-                endpoint = intervals[i][1];
-            }
-         }
-         return answer;
-
+        return answer;
     }
 }

51e50454e8e14e73739a2a270f4abe2738b788a5
JIUNG GU <60885635+JIUNG9@users.noreply.github.com>
2025-01-21 21:05:08 +0900

    Time: 17 ms (12.86%), Space: 45.3 MB (64.92%) - LeetHub

--- 0649-dota2-senate/0649-dota2-senate.java
+++ 0649-dota2-senate/0649-dota2-senate.java
@@ -1,26 +1,32 @@
 class Solution {
-    public String predictPartyVictory(String s) {
-        Queue<Integer> q1 = new LinkedList<>();
-        Queue<Integer> q2 = new LinkedList<>();
-        int idx = 0;
-        for(char c : s.toCharArray()){
-            if(c == 'R') q1.add(idx++);
-            else{
-                q2.add(idx++);
-            }
+    public String predictPartyVictory(String senate) {
+        Queue<Character> banRight = new LinkedList<>();
+        Queue<Character> alive = new LinkedList<>();
+        
+        while(banRight.size() <= 0){
+            for(char c : senate.toCharArray()){
+                if(!banRight.isEmpty() && banRight.peek()!=c){
+                    banRight.poll();
+                }
+                else{
+                    banRight.add(c);
+                    alive.add(c);
+                }
+                }
+                StringBuilder sb = new StringBuilder();
+                while(!alive.isEmpty()){
+                    if(!banRight.isEmpty() && banRight.peek()!=alive.peek()){
+                        alive.poll();
+                        banRight.poll();
+                    }
+                    else{
+                        sb.append(String.valueOf(alive.poll()));
+                    }
+                }
+                senate = sb.toString();
+                System.out.println("senate: " + senate);
+
         }
-
-        while(!q1.isEmpty() && !q2.isEmpty()){
-            if(q1.peek() < q2.peek()){
-                q1.add(idx++);
-            }
-            else{
-                q2.add(idx++);
-            }
-            q1.poll();
-            q2.poll();
-        }   
-        return q1.isEmpty() ? "Dire" : "Radiant";
-
+        return banRight.poll().equals('D') ? "Dire" : "Radiant";
     }
 }

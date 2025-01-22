630188c08c5e6ede25a1505ee0461c38049fb146
JIUNG GU <60885635+JIUNG9@users.noreply.github.com>
2025-01-22 10:39:46 +0900

    Time: 119 ms (5.16%), Space: 45.4 MB (5.82%) - LeetHub

--- 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.java
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.java
@@ -1,11 +1,28 @@
+import java.util.*;
+import java.util.regex.*;
+
 class Solution {
     public String gcdOfStrings(String str1, String str2) {
-        if(!str1.concat(str2).equals(str2.concat(str1))) return "";
-        int res = gcd(str1.length(), str2.length());
-        return str1.substring(0,res);
+        if (str1.length() < str2.length()) {
+            String temp = str1;
+            str1 = str2;
+            str2 = temp;
+        }
 
+        for (int i = str2.length(); i > 0; i--) {
+            String candidate = str2.substring(0, i);
+            if (isGCD(candidate, str1) && isGCD(candidate, str2)) {
+                return candidate;
+            }
+        }
+        return "";
     }
-    public int gcd(int a,int b){
-        return b == 0 ? a : gcd(b,a%b);
+
+    public boolean isGCD(String pat, String input) {
+        int times = input.length() / pat.length();
+        String regex = "(" + Pattern.quote(pat) + "){" + times + "}";
+        Pattern p = Pattern.compile(regex);
+        Matcher m = p.matcher(input);
+        return m.matches();
     }
 }

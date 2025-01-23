eb67e1c3820427f19fbc3a875abe013027ee035e
JIUNG GU <60885635+JIUNG9@users.noreply.github.com>
2025-01-23 09:42:49 +0900

    Time: 0 ms (100%), Space: 41.1 MB (59.19%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -2,7 +2,6 @@ class Solution {
     public int rob(int[] nums) {
         if(nums.length == 1) return nums[0];
         else if(nums.length == 2) return Math.max(nums[0], nums[1]);
-        else if(nums.length == 3) return Math.max(Math.max(nums[0], nums[1]),nums[2] + nums[0]);
 
         int[] dp = new int[nums.length];
 

b79b578c83a7b1d42a3773f6317de93cdba7a41d
JIUNG GU <60885635+JIUNG9@users.noreply.github.com>
2025-01-26 21:06:23 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.java
+++ 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.java
@@ -1,28 +1,32 @@
 class Solution {
-    private List<Character> vowels = List.of('a','e','i','o','u');
+    
     public int maxVowels(String s, int k) {
-        int left = 0;
+        int[] arr = new int[26];
+        int count = 0;
+        int answer = 0;
+        arr['a'-97]++;
+        arr['e' - 97]++;
+        arr['i' - 97]++;
+        arr['o' - 97]++;
+        arr['u' - 97]++;
+
+    
+
         int right = 0;
-        int len = s.length();
-        int counter = 0;
-        int answer =0;
+        int left = 0;
 
-        while(right< k){
-            if(vowels.contains(s.charAt(right++))) counter++;
-        }
-        right--;
+        while(right < s.length())
+        {
+            while(right < k){
+                if(arr[s.charAt(right++) - 97] == 1) count++;
+            }
+            
+            answer = Math.max(count, answer);
 
-        answer = Math.max(answer,counter);
+            if(arr[s.charAt(left++) - 97] == 1) count--;
+            if(arr[s.charAt(right++) - 97] == 1) count++;
 
-        while(right + 1 < len){
-            char leftC = s.charAt(left);
-            char movedRightC = s.charAt(right + 1);
-            if(vowels.contains(leftC)) counter--;
-            if(vowels.contains(movedRightC)) counter++;
-            answer = answer = Math.max(answer,counter);
-            left++;
-            right++;
-
+            answer = Math.max(count, answer);
         }
         return answer;
     }
